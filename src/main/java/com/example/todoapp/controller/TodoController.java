66f879bcfcf026d9099edd3ab21baac14fa6b607
kimsha-it <kimsha0it@gmail.com>
2025-11-18 11:24:54 +0900

    read 1 로직 작성

--- src/main/java/com/example/todoapp/controller/TodoController.java
+++ src/main/java/com/example/todoapp/controller/TodoController.java
@@ -5,6 +5,7 @@ import com.example.todoapp.repository.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -40,5 +41,12 @@ public class TodoController {
         model.addAttribute("todo", todo);
 //        return "create";
         return "redirect:/todos";
+    }
+
+    @GetMapping("/todos/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        TodoDto todo = todoRepository.findById(id);
+        model.addAttribute("todo", todo);
+        return "detail";
     }
 }
