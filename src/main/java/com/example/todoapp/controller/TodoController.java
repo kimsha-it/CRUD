a4420c963fa459b0577e1858b3e7486d85595cdc
kimsha-it <kimsha0it@gmail.com>
2025-11-18 10:35:00 +0900

    create

--- src/main/java/com/example/todoapp/controller/TodoController.java
+++ src/main/java/com/example/todoapp/controller/TodoController.java
@@ -1,12 +1,35 @@
 package com.example.todoapp.controller;
 
+import com.example.todoapp.dto.TodoDto;
+import com.example.todoapp.repository.TodoRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
     @GetMapping("/todos")
     public String todos(){
         return "todos";
+    }
+
+    @GetMapping("/todos/new")
+    public String newTodo(){ //new 단축어 있어서 newTodo로 대체.
+        return "new";
+    }
+
+    @GetMapping("/todos/create")
+    public String create(
+            @RequestParam String title,
+            @RequestParam String content,
+            Model model
+    ){
+        TodoDto todoDto = new TodoDto(null, title, content, false);
+        TodoRepository todoRepository = new TodoRepository();
+
+        TodoDto todo = todoRepository.save(todoDto);
+        model.addAttribute("todo", todo);
+        return "create";
     }
 }
